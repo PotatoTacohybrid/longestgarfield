0c1cd788654e96d474781990c11f5bf4fa3883f3
Tommy Wisaeu <kingdomkeygaming@gmail.com>
2019-02-05 22:41:49 -0600

    Changed txt file to database

--- application.py
+++ application.py
@@ -4,16 +4,20 @@ import datetime
 
 from flask import Flask, render_template, request, jsonify, redirect
 from flask_socketio import SocketIO, emit
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+engine = create_engine("postgres://jibkkepbuwfmbv:518094e0c3908585dd9b5b2caee32ede587a75896bf35b6b24ad9db9a0a158fe@ec2-107-20-183-142.compute-1.amazonaws.com:5432/dbe86mmth840i0")
+db = scoped_session(sessionmaker(bind=engine))
+
 @app.route("/")
 def index():
 
-    with open('main.txt') as f:
-        main = f.readlines()
+    main = db.execute("SELECT * FROM texts").fetchall()
 
     return render_template("index.html", main=main)
 
@@ -23,8 +27,8 @@ def index():
 def garfield(data):
     newText = data["newText"]
 
-    with open("main.txt", 'a') as f:
-        f.write(f'{newText}\n')
+    db.execute("INSERT INTO texts (text) VALUES (:text)", {"text": newText})
+    db.commit()
 
     emit('add garfield', {'newText': newText}, broadcast=True)
 
